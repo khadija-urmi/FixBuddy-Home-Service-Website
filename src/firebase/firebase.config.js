aa75e37a6ac138458fe113be783d69702d946312
khadija-urmi <khadija5jahan211@gmail.com>
2024-12-26 23:16:21 +0600

    env.local file is added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBFJYausUZHiQnTW7p0vcCreMfoBkOoDRU",
-    authDomain: "home-service-website.firebaseapp.com",
-    projectId: "home-service-website",
-    storageBucket: "home-service-website.firebasestorage.app",
-    messagingSenderId: "38522551478",
-    appId: "1:38522551478:web:c880ffd16349cd1db83a17"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
