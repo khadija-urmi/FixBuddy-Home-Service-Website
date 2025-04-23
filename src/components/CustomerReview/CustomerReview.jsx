c073dc3182f480a395b1506e0a5b97e1ee1fec66
khadija-urmi <khadija5jahan211@gmail.com>
2025-04-23 12:33:06 +0600

    add responsive in home page

--- src/components/CustomerReview/CustomerReview.jsx
+++ src/components/CustomerReview/CustomerReview.jsx
@@ -3,7 +3,7 @@ const CustomerReview = () => {
     {
       text: "Amazing service! Highly recommend.",
       name: "John Doe",
-      avatar: "https://i.ibb.co.com/9vdVC7F/admin-2.png",
+      avatar: "https://i.ibb.co.com/hxYJSWkk/1.jpg",
     },
     {
       text: "Professional and timely delivery!",
@@ -13,38 +13,41 @@ const CustomerReview = () => {
     {
       text: "Outstanding support and quality work!",
       name: "Emily Johnson",
-      avatar: "https://i.ibb.co.com/9vdVC7F/admin-2.png",
+      avatar: "https://i.ibb.co.com/JwK5D9Zf/2.jpg",
     },
     {
       text: "Great experience, I will use their services again.",
       name: "Michael Brown",
-      avatar: "https://i.ibb.co.com/9vdVC7F/admin-2.png",
+      avatar: "https://i.ibb.co.com/46gLvwt/3.jpg",
     },
     {
       text: "Reliable and affordable services.",
       name: "Sarah Davis",
-      avatar: "https://i.ibb.co.com/9vdVC7F/admin-2.png",
+      avatar: "https://i.ibb.co.com/fGVvjvHc/6.jpg",
     },
     {
       text: "So friendly & maintain a hygienic",
       name: "Sara Ali Khan",
-      avatar: "https://i.ibb.co.com/9vdVC7F/admin-2.png",
+      avatar: "https://i.ibb.co.com/RTjwK4NP/7.jpg",
     },
   ];
 
   return (
     <div className="mt-16 w-full lg:w-11/12 mx-auto  py-20 px-8">
-      <div className="w-2/5 mx-auto divider divider-accent text-3xl font-bold text-center ">
+      <div className="w-2/5 mx-auto divider divider-accent text-xl lg:text-3xl font-bold text-center ">
         <span>What Our Clients Say</span>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-6 mt-8">
         {reviews.map((review, index) => (
-          <div key={index} className="bg-white p-6 shadow-md rounded-md">
+          <div
+            key={index}
+            className="bg-white p-6 shadow-lg   border-2  border-l-blue-300 rounded-md"
+          >
             <div className="flex items-center mb-4">
               <img
                 src={review.avatar}
                 alt={review.name}
-                className="w-12 h-12 rounded-full mr-4"
+                className="w-14 h-14 rounded-full mr-4"
               />
               <p className="font-bold">{review.name}</p>
             </div>
